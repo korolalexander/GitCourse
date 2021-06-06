dd570154afce64c6d0bdfec3b4003213f0008ad2
Alexander Korol <alexander.korol@vrpconsulting.com>
2021-06-06 23:14:53 +0300

    new commit

--- AccountDetailInfoController.js
+++ AccountDetailInfoController.js
@@ -7,5 +7,5 @@
     clearCard : function (component, event, helper) {
         let params = event.getParam('arguments');
         if(params) helper.clearRecord(component, helper, params.isSure);
-    },
+    }
 });
